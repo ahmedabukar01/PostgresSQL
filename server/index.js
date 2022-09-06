8f56dfd1e73745faf99cb7073ff8144b55d76d26
ahmed <ahmedlearn01@gmail.com>
2022-09-06 17:00:40 +0300

    added

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.post("/todos", async (req,res)=>{
     try {
         const {description} = req.body;
         const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
-        res.json(newTodo)
+        res.json(newTodo.rows[0])
     } catch (error) {
         console.error(error.message)
     }
