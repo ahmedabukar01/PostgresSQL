fae15812bf33d4c3f49c7eaeab0bbba3d9564a8a
ahmed <ahmedlearn01@gmail.com>
2022-09-06 11:22:23 +0300

    postgress setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express  = require('express');
 const cors = require('cors')
+require('dotenv').config();
+const pool = require('./db')
 
 const app = express();
 
 // midlewares
 app.use(cors);
 app.use(express.json());
+
+// create todo 
+
+// get all todo 
+
+// update todo  
+
+// delete todo
 
 app.listen(5000,()=>{
     console.log('SEVER IS RUNNING ');
