1b8f0d9e4ad80ed673b019f341e14247c205cb5e
ahmed <ahmedlearn01@gmail.com>
2022-09-06 11:56:56 +0300

    starting point

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 require('dotenv').config();
 const pool = require('./db')
 
+const port = 8000;
 const app = express();
 
 // midlewares
@@ -10,13 +11,24 @@ app.use(cors);
 app.use(express.json());
 
 // create todo 
-
+app.post('/todos', async (req,res)=>{
+    try {
+        console.log(req.body);
+        res.write('got it')
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 // get all todo 
 
 // update todo  
 
 // delete todo
 
-app.listen(5000,()=>{
-    console.log('SEVER IS RUNNING ');
+app.get('/hey',(req,res)=>{
+    console.log('hi')
+})
+
+app.listen(port,()=>{
+    console.log('SEVER IS RUNNING ', port);
 })
