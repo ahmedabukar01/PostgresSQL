e6b8715be94c98ea95cce93a1f8976d6eb8b53f7
ahmed <ahmedlearn01@gmail.com>
2022-09-07 10:09:42 +0300

    updated

--- server/index.js
+++ server/index.js
@@ -32,12 +32,32 @@ app.get('/todos', async (req,res)=>{
     console.error(error.message)
    }
 })
-// update todo  
+// Single todo
 app.get('/todos/:id', async(req,res)=>{
+   try {
     const { id } = req.params;
     const singleTodo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
 
     res.json(singleTodo.rows);
+   } catch (error) {
+    console.error(error.message)
+   }
+})
+
+// update todo
+app.put('/todos/:id', async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const {description} = req.body;
+        const updated = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description,id])
+
+        const updatedDesc = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+
+        res.json({message: "updated", updated: updatedDesc.rows[0]})
+    } catch (error) {
+        console.error(error.message)
+        
+    }
 })
 
 // delete todo
